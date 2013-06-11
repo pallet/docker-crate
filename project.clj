7f73d5f2ecf52d09e0b3528c3fcb5e5e6b6ebfe2
Hugo Duncan <hugo@hugoduncan.org>
2013-06-11 09:31:30 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/docker-crate "0.8.0-alpha.1"
+(defproject com.palletops/docker-crate "0.8.0-SNAPSHOT"
   :description "Crate for docker installation"
   :url "http://github.com/pallet/docker-crate"
   :license {:name "Eclipse Public License"
