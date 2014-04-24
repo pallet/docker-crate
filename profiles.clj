3f805f5d8caf61d7b95426857e561cff3a436ef8
Hugo Duncan <hugo@hugoduncan.org>
2014-04-24 16:39:55 -0400

    Update for travis and release configuration

--- profiles.clj
+++ profiles.clj
@@ -1,17 +1,11 @@
 {:dev
- {:dependencies [[com.palletops/pallet "0.8.0-RC.4" :classifier "tests"]
+ {:dependencies [[com.palletops/pallet "0.8.0-RC.9" :classifier "tests"]
                  [com.palletops/crates "0.1.1"]
                  [ch.qos.logback/logback-classic "1.0.9"]]
-  :plugins [[com.palletops/pallet-lein "0.8.0-alpha.1"]
-            [com.palletops/lein-pallet-crate "0.1.0"]
-            [lein-set-version "0.3.0"]
-            [lein-resource "0.3.2"]
+  :plugins [[com.palletops/lein-pallet-crate "0.1.0"]
             [codox/codox.leiningen "0.6.4"]
-            [lein-marginalia "0.7.1"]]}
- :test
- {:dependencies [[com.palletops/pallet "0.8.0-RC.1" :classifier "tests"]
-                 [ch.qos.logback/logback-classic "1.0.9"]]
-  :resource-paths ["resources" "dev-resources"]}
+            [lein-marginalia "0.7.1"]
+            [lein-pallet-release "RELEASE"]]}
  :doc {:dependencies [[com.palletops/pallet-codox "0.1.0"]]
        :plugins [[codox/codox.leiningen "0.6.4"]
                  [lein-marginalia "0.7.1"]]
@@ -21,19 +15,4 @@
                :src-linenum-anchor-prefix "L"}
        :aliases {"marg" ["marg" "-d" "doc/0.8/annotated"]
                  "codox" ["doc"]
-                 "doc" ["do" "codox," "marg"]}
-       }
- :pallet {:jvm-opts ["-Djna.nosys=true"]}
- :release
- {:set-version
-  {:updates [{:path "README.md" :no-snapshot true}]}}
- :jclouds {:repositories
-           {"sonatype"
-            "https://oss.sonatype.org/content/repositories/releases/"}
-           :dependencies [[org.cloudhoist/pallet-jclouds "1.5.2"]
-                          [org.jclouds/jclouds-allblobstore "1.5.5"]
-                          [org.jclouds/jclouds-allcompute "1.5.5"]
-                          [org.jclouds.driver/jclouds-slf4j "1.5.5"
-                           :exclusions [org.slf4j/slf4j-api]]
-                          [org.jclouds.driver/jclouds-sshj "1.5.5"]]}
- :vmfest {:dependencies [[com.palletops/pallet-vmfest "0.3.0-beta.2"]]}}
+                 "doc" ["do" "codox," "marg"]}}}
